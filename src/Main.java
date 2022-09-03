2dbee25a4eea4ebf7849e304267652eba6a4f460
Baris Kerem Kurt <bariskerem.kurt@gmail.com>
2022-09-03 18:28:49 +0300

    charAt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Kerem";
+        String studentLastName = "KURT";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+
         System.out.println("Hello world from the intelliJ!");
     }
 }
