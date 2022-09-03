56ba145fb700c54431532e80809a214869d8bbea
Baris Kerem Kurt <bariskerem.kurt@gmail.com>
2022-09-03 18:40:06 +0300

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,7 +17,14 @@ public class Main {
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
 
-        System.out.println("Hello world from the intelliJ!");
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName +
+                " now has a GPA of " + studentGPA);
     }
 }
