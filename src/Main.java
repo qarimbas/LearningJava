9e813f4bd70864def3209d686d1149ebad9f269d
Baris Kerem Kurt <bariskerem.kurt@gmail.com>
2022-09-03 19:39:45 +0300

    Control Flow

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,36 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
+        String question = "What is the largest planet in our solar system?";
+        String choiceOne = "earth";
+        String choiceTwo = "jupiter";
+        String choiceThree = "saturn";
 
-        int studentAge = 15;
-        double studentGPA = 3.45;
-        boolean hasPerfectAttendance = true;
-        String studentFirstName = "Kerem";
-        String studentLastName = "KURT";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
+        String correctAnswer = choiceTwo;
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
-        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        // Write a print statement asking the question
+        System.out.println(question);
 
-        System.out.println("What do you want to update it to?");
+        // Write a print statement giving the answer choices
+        System.out.println("Choose one of the following: " +
+                choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
 
-        Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
+        // Have the user input an answer
+        Scanner scanner = new Scanner(System.in);
+        // Retrieve the user's input
+        String input = scanner.next();
 
-        System.out.println(studentFirstName + " " + studentLastName +
-                " now has a GPA of " + studentGPA);
+        // If the user's input matches the correctAnswer...
+        // then the user is correct and we want to print out a congrats message to the user.
+        if(correctAnswer.equals(input.toLowerCase())) {
+            System.out.println("Congrats! That's the correct answer");
+        } else {
+            System.out.println("You are incorrect. The correct answer is " + correctAnswer);
+        }
+        // If the user's input does not match the correctAnswer...
+        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
+
     }
+
 }
